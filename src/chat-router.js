0a97d3a898f681f6b965aa62883c9535eb0b22d6
黄原寅 <3443718488@qq.com>
2024-05-16 10:13:52 +0800

    handle error and abort

--- src/chat-router.js
+++ src/chat-router.js
@@ -9,12 +9,31 @@ const openai = new OpenAI({
 })
 
 router.get('/api/gpt/chat', async (ctx, next) => {
+  ctx.status = 200
+  ctx.set('Content-Type', 'text/event-stream') // 'text/event-stream' 标识 SSE 即 Server-Sent Events
+
+  let gptStream
+
+  ctx.req.on('close', () => {
+    console.log('req close...')
+
+    // 取消请求
+    console.log('try abort...')
+    if (!ctx.gptStreamDone) {
+      console.log('abort request...')
+      gptStream.controller.abort()
+      console.log('abort ok~ ')
+    }
+  })
+
   const query = ctx.query || {}
 
   // 简单的密钥
   const authToken = query['x-auth-token'] || ''
   if (!authToken.trim() || authToken !== process.env.AUTH_TOKEN) {
-    ctx.body = 'invalid token'
+    const errMsg = 'invalid token'
+    console.log('error: ', errMsg)
+    ctx.res.write(`data: [ERROR]${errMsg}\n\n`) // 格式必须是 `data: xxx\n\n` ！！！
     return
   }
 
@@ -24,35 +43,45 @@ router.get('/api/gpt/chat', async (ctx, next) => {
   const option = JSON.parse(decodeOptionStr)
 
   if (!option.messages) {
-    ctx.body = 'invalid option: messages required'
+    const errMsg = 'invalid option: messages required'
+    console.log('error: ', errMsg)
+    ctx.res.write(`data: [ERROR]${errMsg}\n\n`)
     return
   }
 
-  // request GPT API
-  const gptStream = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    // messages: [{ role: 'user', content: 'xxx' }],
-    // max_tokens: 100,
-    stream: true, // stream
-    ...option,
-  })
+  try {
+    // request GPT API
+    gptStream = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      // messages: [{ role: 'user', content: 'xxx' }],
+      // max_tokens: 100,
+      stream: true, // stream
+      ...option,
+    })
+  } catch (err) {
+    const errMsg = err.message || 'request openai API error'
+    console.log('error: ', errMsg)
+    ctx.res.write(`data: [ERROR]${ex.message}\n\n`)
+    return
+  }
 
-  ctx.status = 200
-  ctx.set('Content-Type', 'text/event-stream') // 'text/event-stream' 标识 SSE 即 Server-Sent Events
+  if (gptStream == null) {
+    const errMsg = 'gptStream is not defined'
+    console.log('error: ', errMsg)
+    ctx.res.write(`data: [ERROR]${errMsg}\n\n`)
+    return
+  }
 
   for await (const chunk of gptStream) {
     const content = chunk.choices[0].delta.content
     console.log('content: ', content)
     if (content == null) {
+      ctx.gptStreamDone = true
       ctx.res.write(`data: [DONE]\n\n`)
       break
     }
     ctx.res.write(`data: ${content}\n\n`) // 格式必须是 `data: xxx\n\n` ！！！
   }
-
-  ctx.req.on('close', () => {
-    console.log('req close...')
-  })
 })
 
 module.exports = router
